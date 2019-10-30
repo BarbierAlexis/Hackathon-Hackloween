3a80c6760ab508e43abc147d6d2eab2e45b70e92
Alexis Barbier <barbier-alexis@hotmail.fr>
2019-10-31 00:40:33 +0100

    Add API call

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ const config = {
 	}
 }
 
+let helloWorld = () => {
+
+		fetch.get(`https://hackathon-wild-hackoween.herokuapp.com/`) 
+		.then(response => response.json())
+		.then((response) =>{
+			console.log(response)
+		})
+	}
+
+
 let game = new Phaser.Game(config)
 let perso
 let cursors
@@ -37,15 +47,16 @@ function create(){
 	//hero = this.physics.add.image(400,150, 'hero').setScale(0.8)
 	//hero.body.collideWorldBounds = true;
 
-
-	let group = this.physics.add.group({ key: 'hero', frameQuantity: 10 });
-	let rect = new Phaser.Geom.Rectangle(0, 0, 500, 300);
+	let group = this.physics.add.group({ key: 'hero', frameQuantity: 20 });
+	let rect = new Phaser.Geom.Rectangle(0, 0, 500, 300);	
 	Phaser.Actions.RandomRectangle(group.getChildren(), rect);
+	this.physics.add.overlap(perso, group, kill, null, this);
+	
 
 	//this.physics.add.collider(perso,hero)  ==> Creer une collision
 
 	this.physics.add.overlap(perso, hero, kill, null, this);
-	this.physics.add.overlap(perso, group, kill, null, this);
+	
 
 }
 
@@ -53,6 +64,7 @@ function update(){
 	
 	//hero.setVelocity(0)
    
+	
 
 	perso.setVelocity(0)
 
