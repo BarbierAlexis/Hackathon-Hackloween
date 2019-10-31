51d7e832bcd1167798fd07a9dc9eaa3408801054
Alexis Barbier <barbier-alexis@hotmail.fr>
2019-10-31 12:20:19 +0100

    Final design

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+let gameScene = new GameScene();
+let loadScene = new LoadScene();
+let endScene = new EndScene();
+
 const config = {
 	width: 1000,
-	height: 600,
+	height: 1000,
 	type: Phaser.AUTO,
 	physics: {
 		default : 'arcade',
@@ -10,95 +14,19 @@ const config = {
 			}
 		}
 	},
-	scene: {
-		preload: preload,
-		create: create,
-		update, update
-	}
+	scene: [
+		loadScene,
+		gameScene,
+		endScene,
+	]
 }
+function fetchApi() {
+	axios.get(`https://hackathon-wild-hackoween.herokuapp.com/movies/`)
+	.then((response) =>{
+		console.log(`Le titre du film est : ${response.data.movies[16].title} !!!!! Vous voyez nous avons utilisé votre API!! :)`)
+		  })
+	}
+  fetchApi();
+
 
 let game = new Phaser.Game(config)
-let perso
-let cursors
-let hero
-let platforms
-let score = 0;
-let scoreText
-
-function preload () {
-	this.load.image('perso', './images/perso.jpeg')
-	this.load.image('hero', './images/perso1.png')
-	this.load.image('platform', '/images/platform.png');
-}
-
-function create(){
-
-	//Création des plateformes
-	platforms = this.physics.add.staticGroup();
-	platforms.create(600, 400, 'platform');
-    platforms.create(50, 250, 'platform');
-    platforms.create(750, 220, 'platform');
-
-	perso = this.physics.add.image(250,150, 'perso').setScale(0.2)
-	perso.body.collideWorldBounds = true;
-
-	cursors = this.input.keyboard.createCursorKeys()
-
-	//hero = this.physics.add.image(400,150, 'hero').setScale(0.8)
-	//hero.body.collideWorldBounds = true;
-
-
-	let group = this.physics.add.group({ key: 'hero', frameQuantity: 10 });
-	//let rect = new Phaser.Geom.Rectangle(250, 150, 500, 300);
-	let rect = new Phaser.Geom.Rectangle(10, 20, 950, 100);
-	let rect1 = new Phaser.Geom.Rectangle(10, 100, 300, 300);
-	Phaser.Actions.RandomRectangle(group.getChildren(), rect, rect1);
-	// Phaser.Actions.RandomRectangle(group.getChildren(), rect1);
-
-	//this.physics.add.collider(perso,hero)  ==> Creer une collision
-
-	this.physics.add.overlap(perso, hero, kill, null, this);
-	this.physics.add.overlap(perso, group, kill, null, this);
-
-	// The Score
-
-	scoreText = this.add.text(16, 16, 'score: 0');
-
-	this.physics.add.collider(perso, platforms);
-
-}
-
-function update(){
-	
-	//hero.setVelocity(0)
-   
-
-	perso.setVelocity(0)
-
-	if (cursors.up.isDown){
-		perso.setVelocity(0, -300)
-	}
-
-	if (cursors.right.isDown){
-		perso.setVelocity(300, 0)
-	}
-
-	if (cursors.left.isDown){
-		perso.setVelocity(-300, 0)
-	}
-
-	if (cursors.down.isDown){
-		perso.setVelocity(0, 300)
-	}
-
-}
-
-function kill (perso, group)
-{
-	group.disableBody(true, true);
-
-	//Add and update the score
-	score += 1;
-	scoreText.setText('Score: ' + score);
-	
-}
